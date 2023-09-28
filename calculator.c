52a3173aff35c37b55d665560aa4e2342f328a83
Aroge21 <82720686+Aroge21@users.noreply.github.com>
2023-09-28 12:46:27 -0500

    Implemented addition feature

--- calculator.c
+++ calculator.c
@@ -1,5 +1,16 @@
 #include <stdio.h>
+#include <math.h>
+
+double add(double a, double b);
 
 int main(){
-    
+    double a = 3, b = 4, results;
+
+    results = add(a, b);
+    printf("%lf\n", results);
+
+}
+
+double add(double a, double b) {
+    return a + b;
 }
