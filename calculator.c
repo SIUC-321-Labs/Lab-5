d6fcb0ccf0dd5edcb040fd4b3daf98ad6256962b
Aroge21 <82720686+Aroge21@users.noreply.github.com>
2023-09-28 12:49:04 -0500

    Implemented subtraction feature

--- calculator.c
+++ calculator.c
@@ -2,15 +2,20 @@
 #include <math.h>
 
 double add(double a, double b);
+double subtract(double a, double b);
 
 int main(){
     double a = 3, b = 4, results;
 
-    results = add(a, b);
+    results = subtract(a, b);
     printf("%lf\n", results);
 
 }
 
 double add(double a, double b) {
     return a + b;
+}
+
+double subtract(double a, double b) {
+    return a - b;
 }
